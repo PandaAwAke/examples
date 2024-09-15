1ab30efdd28039bd6cb58872a67cdc94fbc7f642
Yingshuo Ma <pandaawake@163.com>
2024-09-15 11:20:22 +0800

    Fix: swagger2 to swagger3

--- SpringCloud/item-service/src/main/java/com/pandaawake/item/ItemApplication.java
+++ SpringCloud/item-service/src/main/java/com/pandaawake/item/ItemApplication.java
@@ -1,12 +1,15 @@
 package com.pandaawake.item;
 
 
+import com.pandaawake.common.config.OpenAPIConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @MapperScan("com.pandaawake.item.mapper")
+@Import(OpenAPIConfig.class)
 public class ItemApplication {
 
     public static void main(String[] args) {
